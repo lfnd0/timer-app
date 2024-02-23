9e4c50d1d1ef04061fad2d09f925dbe324a39726
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-02-23 16:06:24 -0300

    Configurando ESLint

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -20,12 +20,12 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   border: 0;
   margin: 8px;
 
-  /* ${props => {
+  /* ${(props) => {
     return css`
-      background-color: ${buttonVariants[props.variant]}
+      background-color: ${buttonVariants[props.variant]};
     `
   }} */
 
-  background-color: ${props => props.theme['green-500']};
-  color: ${props => props.theme.white};
+  background-color: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme.white};
 `
