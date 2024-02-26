c9e61dae14e1fb1a516abe1c7b228225fe21c5a4
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-02-25 22:10:06 -0300

    Página: Home

---
+++ src/page/Home/index.tsx
@@ -0,0 +1,38 @@
+import { Play } from '@phosphor-icons/react'
+import {
+  CountDownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" />
+
+          <label htmlFor="">durante</label>
+          <input id="minutesAmount" type="number" />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+
+        <button type="submit">
+          <Play />
+          Começar
+        </button>
+      </form>
+    </HomeContainer>
+  )
+}
